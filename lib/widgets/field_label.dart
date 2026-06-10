b9183a54b00988f827aa8dbcae1518cf1a2d387e
sudo-114 <c7306870@gmail.com>
2026-06-10 23:19:20 +0000

    add Patient form for CompleteProfile screen

---
+++ lib/widgets/field_label.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class FieldLabel extends StatelessWidget {
+  final String label;
+  const FieldLabel({super.key, required this.label});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          style: Theme.of(context).textTheme.labelMedium?.copyWith(fontWeight: FontWeight.bold),
+        ),
+        const SizedBox(height: 8),
+      ],
+    );
+  }
+}
